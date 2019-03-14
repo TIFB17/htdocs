2561e5ed7fee453e17c4a0fcd229c852b382da99
hakimhakiki <emhakiki97@gmail.com>
2019-03-14 15:30:57 +0700

    update baru

---
+++ CI.3.E41171041_P1/application/views/view_home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Halaman Home</title>
+</head>
+<body>
+	Selamat datang ke website ini.<br>
+	Anda masuk dengan nama <font color="red" size="14pt"><?php echo $nama;?></font>
+</body>
+</html>
