831160bfde8d7ddb4f0acbb5edac02af14abb180
hakimhakiki <emhakiki97@gmail.com>
2019-03-19 20:13:18 +0700

    punya aldi

---
+++ CI.3.E41170754_P2/tokobuah/application/views/view_belajar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cara Membuat Heading dengan Helper HTML Codeigniter | TokoBuah.com</title>
+</head>
+<body>
+	<?php 
+	echo heading("Helper HTML CodeIgniter | TokoBuah.com",1);
+	echo heading("Helper HTML CodeIgniter | TokoBuah.com",2);
+	echo heading("Helper HTML CodeIgniter | TokoBuah.com",3);
+	echo heading("Helper HTML CodeIgniter | TokoBuah.com",4);
+	echo heading("Helper HTML CodeIgniter | TokoBuah.com",5);
+	echo heading("Helper HTML CodeIgniter | TokoBuah.com",6);
+	?>
+</body>
+</html>
