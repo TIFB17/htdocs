6c880f94b308e1dfb1d1bdf248e4bfb78a928299
hakimhakiki <emhakiki97@gmail.com>
2019-03-19 19:45:45 +0700

    p2 view_controller

---
+++ CI.3.E41171041_P2/application/views/view_belajar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cara membuat view pada Codeigniter</title>
+</head>
+<body>
+	<?php 
+	echo heading("Helper HTML CodeIgniter | tokobuah.com",1);
+	echo heading("Helper HTML CodeIgniter | tokobuah.com",2);
+	echo heading("Helper HTML CodeIgniter | tokobuah.com",3);
+	echo heading("Helper HTML CodeIgniter | tokobuah.com",4);
+	echo heading("Helper HTML CodeIgniter | tokobuah.com",5);
+	echo heading("Helper HTML CodeIgniter | tokobuah.com",6);
+	?>
+</body>
+</html>
