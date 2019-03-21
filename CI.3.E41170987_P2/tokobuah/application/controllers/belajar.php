d45ac5183b74b132282a8a04a62c30a54a380cb0
@novia318 <novvianurul@gmail.com>
2019-03-21 14:48:55 +0700

    p2

--- CI.3.E41170987_P2/tokobuah/application/controllers/belajar.php
+++ CI.3.E41170987_P2/tokobuah/application/controllers/belajar.php
@@ -5,15 +5,21 @@ class Belajar extends CI_Controller {
 	
 	function __construct(){
 		parent::__construct();
+		$this->load->helper('html');
 		
 	}
  
 	public function index(){
 		echo "ini method index pada controller belajar | Cara membuat controller pada codeigniter";
+		//$this->load->view('welcome');
 	}
  
 	public function halo(){
 		echo "ini method halo pada controller belajar | Cara membuat controller pada codeigniter";
+			//$this->load->view('view_belajar');
+		$data['nama_web'] = "TokoBuah.com";
+		$this->load->view('view_belajar',$data);
+
 	}
  
 }
