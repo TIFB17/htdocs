a39edb3c3bd7c23088ee9175240abfae41a2017d
abdilanamohtalia <44762945+abdilanamohtalia@users.noreply.github.com>
2019-03-21 15:02:59 +0700

    abdi punya
    punya saya

--- CI.3.E41170740_P2/tokobuah/application/controllers/belajar2.php
+++ CI.3.E41170740_P2/tokobuah/application/controllers/belajar2.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
  
-class Belajar extends CI_Controller {
+class Belajar2 extends CI_Controller {
 	
 	function __construct(){
 		parent::__construct();
@@ -9,6 +9,6 @@ class Belajar extends CI_Controller {
 	}
  
 	public function index(){
-		$this->load->view('view_belajar');
+		$this->load->view('view_belajar2');
 	}	
 }
